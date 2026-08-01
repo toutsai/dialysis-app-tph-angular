77b1afc6229237fe7e8291c814312228a744b074
dialysis-admin <suiam74@gmail.com>
2026-08-02 03:16:40 +0800

    調班留言拿掉🔁圖示:排程/護理分組改班資訊由格子備註(換班)(臨時加洗)(互調)呈現即可

--- angular-client/src/app/components/patient-messages-icon/patient-messages-icon.component.ts
+++ angular-client/src/app/components/patient-messages-icon/patient-messages-icon.component.ts
@@ -21,7 +21,6 @@ export class PatientMessagesIconComponent {
       case '抽血': return '🩸';
       case '衛教': return '📢';
       case 'record': return '🩺';
-      case '調班': return '🔁';
       case 'memo':
       case '常規':
       default: return '📝';
@@ -33,7 +32,6 @@ export class PatientMessagesIconComponent {
       case '抽血': return '有抽血提醒';
       case '衛教': return '有衛教事項';
       case 'record': return '有病情紀錄';
-      case '調班': return '有調班交接';
       case 'memo':
       case '常規':
       default: return '有交班事項';
@@ -42,10 +40,11 @@ export class PatientMessagesIconComponent {
 
   get computedMessageTypes(): string[] {
     if (!this.patientId) return [];
-    if (this.typesMap && this.typesMap instanceof Map) {
-      return this.typesMap.get(this.patientId) || [];
-    }
-    return this.messageTypes;
+    const types = (this.typesMap && this.typesMap instanceof Map)
+      ? this.typesMap.get(this.patientId) || []
+      : this.messageTypes;
+    // 調班留言不出圖示：改班資訊由排程格備註 (換班)/(臨時加洗)/(互調) 呈現即可
+    return types.filter((type) => type !== '調班');
   }
 
   handleClick(type: string): void {
